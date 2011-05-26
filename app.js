93250b17ebc8b97afb7807f70c6476f612557cf7
Ruben de Vries <rubensayshi@gmail.com>
2011-05-26 20:43:19 +0000

    using a base template and removed the dummy stuff

--- app.js
+++ app.js
@@ -7,47 +7,25 @@ var sys 			= require('sys'),
 //set template path
 template_loader.set_path('templates');
 
-//context to use when rendering template. In a real app this would likely come from a database
-var test_context = {
-person_name: 'Thomas Hest',
-company: 'Tobis A/S',
-ship_date: new Date('12-02-1981'),
-item: 'XXX',
-item_list: [ 'Giraf', 'Fisk', 'Tapir'],
-ordered_warranty: true,
-ship: {
-    name: 'M/S Martha',
-    nationality: 'Danish'
-}
-};
+// make static files available
+app.use("/web", express.static(__dirname + '/web'));
 
+// require controllers
 require('./controllers/textpage')(app);
 
-app.get('/', function(req, res){
-    res.send('<h1>express + django template demo</h1> \
-            <ul> \
-            <li><a href="/text">The template rendered as text</a></li> \
-            <li><a href="/html">The template rendered as html</a></li> \
-            <li><a href="/page">page</a></li> \
-            <li><a href="/pagee">page</a></li> \
-            </ul> \
-    ');
+// add some more routing
+app.get('/', function(req, res){    
+	template_loader.load_and_render('homepage.html', {}, function (error, result) {
+	    if (error) {
+	        console.log(error);
+	    } else {
+	    	res.send(result);
+	    }
+	});
 });
 
-app.get('/text', function(req, res){
-    template_loader.load_and_render('template.html', test_context, function (error, result) {
-        if (error) {
-            console.log(error);
-        } else {
-            res.writeHeader(200, { 'Content-Type': 'text/plain;' });
-        	res.write(result);
-        	res.end();
-        }
-    });
-});
-
-app.get('/html', function(req, res){
-    template_loader.load_and_render('template.html', test_context, function (error, result) {
+app.get('/about', function(req, res){
+    template_loader.load_and_render('about.html', {}, function (error, result) {
         if (error) {
             console.log(error);
         } else {
@@ -56,4 +34,5 @@ app.get('/html', function(req, res){
     });
 });
 
+// listen
 app.listen(3000);

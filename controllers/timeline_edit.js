c8874a86539d3773307e816eb308a48985b68285
Ruben de Vries <rubensayshi@gmail.com>
2011-06-05 20:41:51 +0000

    displaying and handling forms

--- controllers/timeline_edit.js
+++ controllers/timeline_edit.js
@@ -9,7 +9,7 @@ var core				= require('../lib/core'),
 module.exports = exports = function(app) {	
 	app.get('/my_timelines', auth.restrict, function(req, res, next) {	
 		timelines_repo.find({author_id : req.session.user._id}, function(error, docs) {
-			if(error) {
+			if (error) {
 				next(error);
 			} else {
 				core.render('my_timelines.html', {mytimelines: docs}, res);
@@ -23,7 +23,7 @@ module.exports = exports = function(app) {
       			timelines_repo.findOne({slug : req.params['timeline_slug']}, callback);
       		},
       		function(timeline, callback) {      			
-      			if(auth.check_acl(req.session.user, JSON.stringify({edit_timeline: timeline._id}))) {
+      			if (auth.check_acl(req.session.user, JSON.stringify({edit_timeline: timeline._id}))) {
       				callback(null, timeline);
       			} else {
       				callback(new Error('Access denied'));
@@ -36,10 +36,43 @@ module.exports = exports = function(app) {
        			});
       		}
       		], function(error, timeline, timelineItems) {
-				if(error) {
+				if (error) {
 					core.render('plain.html', {header: 'Error', content: error.message}, res);
 				} else {
 					core.render('timeline_edit.html', {timeline: timeline, timelineItems : timelineItems}, res);
+				}
+			}
+		);
+	});
+	app.post('/timeline/save/:timeline_slug', auth.restrict, function(req, res) {
+		async.waterfall([
+      		function(callback) {
+      			// fetch categories
+      			timelines_repo.findOne({slug : req.params['timeline_slug']}, callback);
+      		},
+      		function(timeline, callback) {      			
+      			if (auth.check_acl(req.session.user, JSON.stringify({edit_timeline: timeline._id}))) {
+      				callback(null, timeline);
+      			} else {
+      				callback(new Error('Access denied'));
+      			}
+      		},
+      		function(timeline, callback) {      			
+      			if (postdata = req.body.timeline) {
+      				for (prop in postdata) {
+      					timeline[prop] = postdata[prop];
+      				}
+      			}
+      			
+      			timeline.save(function(error, doc) {
+      				callback(error, doc);
+      			});
+      		}
+      		], function(error, timeline) {
+				if (error) {
+					core.render('plain.html', {header: 'Error', content: error.message}, res);
+				} else {
+      				res.redirect('/timeline/edit/' + timeline.slug); 
 				}
 			}
 		);
@@ -51,7 +84,7 @@ module.exports = exports = function(app) {
       			timeline_items_repo.findOne({slug : req.params['timeline_item_slug']}, callback);
       		},
       		function(timeline_item, callback) {      			
-      			if(auth.check_acl(req.session.user, JSON.stringify({edit_timeline: timeline_item.timeline_id}))) {
+      			if (auth.check_acl(req.session.user, JSON.stringify({edit_timeline: timeline_item.timeline_id}))) {
       				callback(null, timeline_item);
       			} else {
       				callback(new Error('Access denied'));
@@ -69,12 +102,12 @@ module.exports = exports = function(app) {
    				var curr = false;
    				
    				async.forEachSeries(timelineItems, function(doc, callback) {
-   					if(next) 
+   					if (next) 
    						return callback();
    					       					
    					if (doc._id.toString() == timelineItem._id.toString()) {
    						curr = true;
-   					} else if(curr) {
+   					} else if (curr) {
    						next = doc;
    					} else {       					
    						prev = doc;  
@@ -84,10 +117,43 @@ module.exports = exports = function(app) {
    				}, function(error) { callback(error, timelineItem, timelineItems, prev, next); });    	       		
        		}
       		], function(error, timelineItem, timelineItems, prev, next) {
-				if(error) {
+				if (error) {
 					core.render('plain.html', {header: 'Error', content: error.message}, res);
 				} else {
 					core.render('timeline_item_edit.html', {timelineItem : timelineItem, timelineItems : timelineItems, prev : prev, next : next}, res);
+				}
+			}
+		);
+	});
+	app.post('/timeline_item/save/:timeline_item_slug', auth.restrict, function(req, res) {
+		async.waterfall([
+      		function(callback) {
+      			// fetch categories
+      			timeline_items_repo.findOne({slug : req.params['timeline_item_slug']}, callback);
+      		},
+      		function(timeline_item, callback) {      			
+      			if (auth.check_acl(req.session.user, JSON.stringify({edit_timeline: timeline_item.timeline_id}))) {
+      				callback(null, timeline_item);
+      			} else {
+      				callback(new Error('Access denied'));
+      			}
+      		},
+      		function(timeline_item, callback) {   
+      			if (postdata = req.body.timeline_item) {
+      				for (prop in postdata) {
+      					timeline_item[prop] = postdata[prop];
+      				}
+      			}
+      			
+      			timeline_item.save(function(error, doc) {
+      				callback(error, doc);
+      			});
+      		}
+      		], function(error, timeline_item) {
+				if (error) {
+					core.render('plain.html', {header: 'Error', content: error.message}, res);
+				} else {
+      				res.redirect('/timeline_item/edit/' + timeline_item.slug); 
 				}
 			}
 		);
